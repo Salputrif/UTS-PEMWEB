8a82746d87e3e7e76f6c7fe4fddc6e2c13d9f3d8
Salputrif <90301030+Salputrif@users.noreply.github.com>
2021-11-04 23:32:55 +0700

    Add files via upload

---
+++ visimisi.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Sistem Informasi Kelautan</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  
+  <link href="assets/img/LOGO-UPI.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/css/style2.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- Top Bar-->
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a href="sik_kdserang@upi.edu">sik_kdserang@upi.edu</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>(0254) 200277</span></i>
+      </div>
+
+      <div class="cta d-none d-md-flex align-items-center">
+        <a href="https://pmb.upi.edu/" class="scrollto"> PMB UPI</a>
+      </div>
+    </div>
+  </section>
+
+  <!--  Header -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+      <img src="assets/img/LOGO-UPI.png" alt="" width="40" height="40" >
+      <div class="logo">
+        
+        <h1><a href="index.html">Sistem Informasi Kelautan</a></h1>
+        
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="index.php">Beranda</a></li>
+          <li class="dropdown"><a href="#"><span>Profil</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="sejarah.php">Sejarah</a></li>
+                  <li><a href="visimisi.php">Visi Misi</a></li>
+                  <li><a href="organisasi.php">Struktur Organisasi</a></li>
+                  <li><a href="dosen.php">Dosen</a></li>
+                </ul>
+              </li>
+          <li class="dropdown"><a href="#"><span>Akademik</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="kurikulum.php">Kurikulum</a></li>
+                  <li><a href="laboratorium.php">Laboratorium</a></li>
+                </ul>
+              </li>`
+          <li><a class="nav-link scrollto " href="akredetasi.php">Akredetasi</a></li>
+          <li class="dropdown"><a href="#"><span>Download</span> <i class="bi bi-chevron-right"></i></a>
+            <ul>
+              <li><a href="https://drive.google.com/file/d/1QzVIjXXNbZxCYD2rnOdaMWR_7k83amyw/view">SK Syarat Peringkat Akredetasi</a></li>
+              <li><a href="https://drive.google.com/file/d/1CijlD1g5NZH2Jkwvf90TXoUYX3rdcYp9/view">Struktur Kurikulum</a></li>
+              <li><a href="https://drive.google.com/drive/folders/12tzM1qZQ9NG99olZvmA_syL-TH44oXh2">Rencana Pembelajaran (RPS)</a></li>
+              <li><a href="https://drive.google.com/drive/u/1/folders/1ZlC8ZQWGvVBEEoRyfmFhJ83XofXxtgrY">Nilai Perkuliahan</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="faq.php">FAQ</a></li>
+         
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+  
+  <section id="hero">
+    <div class="hero-container" data-aos="fade-up">
+      <h1>Visi Misi</h1>
+      <h2>Home / Profil / Visi Misi</h2>
+      
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row justify-content-end">
+          <div class="col-lg-11">
+            <div class="row justify-content-end">
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+   
+          <div class="col-lg-6 video-box align-self-baseline position-relative">
+            <img src="assets/img/unnamed.jpg" class="img-fluid" alt="">
+            <a href="https://youtu.be/drATQAonOCA" class="glightbox play-btn mb-4"></a>
+          </div>
+
+          <div class="col-lg-6 pt-3 pt-lg-0 content">
+            <h3>VISI</h3>
+            <p >
+              Mengembangkan dan mengimplementasikan sistem informasi untuk mendukung perkembangan teknologi pada bidang kelautan dan menghasilkan lulusan yang berdayasaing serta ahli dalam pengolahan data numerik maupun spatial.
+              
+            </p>
+            <h3>MISI</h3>
+            <ul>
+              <li><i class="bi bi-check"></i> Menyelenggarakan layanan pendidikan dan pengajaran berstandar nasional bidang keahlian sistem informasi kelautan,</li>
+              <li><i class="bi bi-check"></i> Melaksanakan penelitian di bidang sistem informasi kelautan dan inovasinya dalam upaya mengembangkan potensi sumberdaya hayati pesisir dan laut di daerah dan nasional,</li>
+              <li><i class="bi bi-check"></i> Menerapkan teknologi informasi kelautan yang terpadu, produktif dan terukur dalam pendidikan, penelitian, dan pengabdian serta pemberdayaan kepada masyarakat berbasis hasil penelitian.</li>
+              <li><i class="bi bi-check"></i> Mengembangkan dan menyebarluaskan keilmuan dan teknologi bidang kelautan yang berbasis pada kebutuhan masyarakat dan industri.</li>
+            </ul>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+  
+<!-- ======= Footer ======= -->
+
+<footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Sistem Informasi Kelautan</h3>
+            <p>
+              Jl. Ciracas No.38, Serang,  <br>
+              Kec.Serang, Kota Serang,<br>
+              Banten 42116 <br><br>
+              <strong>Phone:</strong> (0254) 200277<br>
+              <strong>Email:</strong><a href="sik_kdserang@upi.edu"> sik_kdserang@upi.edu</a><br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Tautan</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://www.upi.edu/">UPI Bandung</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://kd-serang.upi.edu/">Kampus UPI SErang</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://kd-cibiru.upi.edu/">Kampus UPI Cibiru</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://kd-tasikmalaya.upi.edu/">Kampus UPI Tasikmalaya</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://kd-purwakarta.upi.edu/">Kampus UPI Purwakarta</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://kd-sumedang.upi.edu/">Kampus UPI Sumedang</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Berita</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://berita.upi.edu/dekan-fpbs-upi-prof-tri-indri-hardini-raih-gelar-chevalier-dalam-palmes-academiques-order-of-academic-palms/">Dekan FPBS UPI Prof. Tri Indri Hardini Raih Gelar Chevalier dalam Palmes Academiques (Order of Academic Palms)</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://berita.upi.edu/kerjasama-pui-tvet-rc-upi-dengan-pusat-inovasi-dan-komersialisasi-lppm-unnes/">Kerjasama PUI TVET-RC UPI dengan Pusat Inovasi dan Komersialisasi LPPM UNNES</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://berita.upi.edu/upi-dinobatkan-sebagai-badan-publik-informatif-rektor-sampaikan-apresiasi/">UPI Dinobatkan sebagai Badan Publik Informatif, Rektor Sampaikan Apresiasi</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://berita.upi.edu/talk-show-inovasi-pendidikan-inovasi-pendidikan-wajib-dilakukan/">Talk Show Inovasi Pendidikan: Inovasi Pendidikan Wajib Dilakukan</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://berita.upi.edu/rektor-upi-dalam-talk-show-inovasi-pendidikan-pendidikan-yang-bermutu-adalah-pendidikan-yang-memberikan-impact-maksimal/">Rektor UPI dalam Talk Show Inovasi Pendidikan: Pendidikan yang Bermutu Adalah Pendidikan yang Memberikan Impact Maksimal</a></li>
+            </ul>
+          </div>
+         
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-lg-flex py-4">
+
+      <div class="me-lg-auto text-center text-lg-start">
+        <div class="copyright">Sistem Informasi Kelautan
+          &copy;  <strong><span>2021</span></strong>
+        </div>
+        
+      </div>
+      <div class="social-links text-center text-lg-right pt-3 pt-lg-0">
+      
+        <a href="https://twitter.com/sik_upi" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="https://web.facebook.com/pages/category/Education/Sistem-Informasi-Kelautan-UPI-365811034019480/?_rdc=1&_rdr" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/sik_upi/" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="https://www.youtube.com/channel/UCYkaBAojBx98my-XfoGZ9tA" class="youtube"><i class="bx bxl-youtube"></i></a>
+    
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
